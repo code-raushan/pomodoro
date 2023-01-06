4ba3ececd5bda8472eabf9adc62147acb8729725
Raushan Kumar <raushan.development@gmail.com>
2023-01-06 17:19:25 +0530

    state updates updated

--- src/components/Modes.jsx
+++ src/components/Modes.jsx
@@ -1,12 +1,28 @@
 import React, {useContext} from 'react'
 import GlobalContext from '../Context'
 const Modes = () => {
-    const {mode, setMode}=useContext(GlobalContext)
+    const {mode, setMode, secondsRemaining, setSecondsRemaining, pomodoroTime, shortBreakTime, longBreakTime, setClockStatus, setAction}=useContext(GlobalContext)
+
+    const changeModeAndTimer = (mode)=>{
+      setMode(mode);
+      if(mode==="pomodoro"){
+        setSecondsRemaining(pomodoroTime*60)
+      }else if(mode==='shortBreak'){
+        setSecondsRemaining(shortBreakTime*60)
+      }else if(mode==='longBreak'){
+        setSecondsRemaining(longBreakTime*60)
+      }
+      setClockStatus('stopped')
+      setAction('START')
+    }
+
+
+
   return (
     <div className='Modes'>
-        <div className={mode==='pomodoro'?'mode-on-click':''} onClick={()=>setMode('pomodoro')}>pomodoro</div>
-        <div className={mode==='short-break'?'mode-on-click':''} onClick={()=>setMode('short-break')}>short break</div>
-        <div className={mode==='long-break'?'mode-on-click':''} onClick={()=>setMode('long-break')}>long break</div>
+        <div className={mode==='pomodoro'?'mode-on-click':''} onClick={()=>changeModeAndTimer('pomodoro')}>pomodoro</div>
+        <div className={mode==='shortBreak'?'mode-on-click':''} onClick={()=>changeModeAndTimer('shortBreak')}>short break</div>
+        <div className={mode==='longBreak'?'mode-on-click':''} onClick={()=>changeModeAndTimer('longBreak')}>long break</div>
     </div>
   )
 }
