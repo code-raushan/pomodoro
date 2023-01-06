3b392406b8c95d9a96dc960fea023f1497691a27
Raushan Kumar <raushan.development@gmail.com>
2023-01-06 17:08:50 +0530

    rest states added

--- src/App.js
+++ src/App.js
@@ -7,9 +7,12 @@ function App() {
   const [pomodoroTime, setPomodoroTime]=useState(30)
   const [shortBreakTime, setShortBreakTime]=useState(5)
   const [longBreakTime, setLongBreakTime]=useState(15)
-
+  const [action, setAction]=useState('START')
+  const [clockStatus, setClockStatus]=useState('stopped')
+  const [isModalOpen, setIsModalOpen]=useState(false)
+  const [secondsRemaining, setSecondsRemaining]=useState(1800)
   return (
-    <GlobalContext.Provider value={{mode, setMode, pomodoroTime, setPomodoroTime, shortBreakTime, setShortBreakTime, longBreakTime, setLongBreakTime}}>
+    <GlobalContext.Provider value={{mode, setMode, pomodoroTime, setPomodoroTime, shortBreakTime, setShortBreakTime, longBreakTime, setLongBreakTime, action, setAction, clockStatus, setClockStatus, isModalOpen, setIsModalOpen, secondsRemaining, setSecondsRemaining}}>
       <div className="App">
         <h1 style={{ color: "white" }}>pomodoro</h1>
         <Modes />
